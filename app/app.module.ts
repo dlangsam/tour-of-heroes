868621eaae944341b21fbb6b55199f913cfe12a5
Devorah Langsam <devorah@langsam.org>
2016-11-14 09:34:34 -0500

    Adding routes

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,50 @@
-import { NgModule }      from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { RouterModule }   from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+import { DashboardComponent }         from './dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+ 		 path: '',
+  		redirectTo: '/dashboard',
+  		pathMatch: 'full'
+	},
+	{
+ 		 path: 'dashboard',
+  		component: DashboardComponent
+	},{
+  		path: 'detail/:id',
+  		component: HeroDetailComponent
+	}
+
+
+   ])
+  ],
+  declarations: [
+    AppComponent,
+    HeroDetailComponent,
+    DashboardComponent,
+    HeroesComponent
+  ],
+  providers: [
+    HeroService
+  ],
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
+
